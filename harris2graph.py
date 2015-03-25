dc55291f18386e6e93b4d56fb841d111d1cc6888
John Layt <jlayt@kde.org>
2015-03-25 11:19:09 +0000

    Add more command line options

--- harris2graph.py
+++ harris2graph.py
@@ -2,12 +2,9 @@
 # -*- coding: utf-8 -*-
 """
 /***************************************************************************
-                                      Ark
-                                 A QGIS plugin
-             QGIS Plugin for ARK, the Archaeological Recording Kit
+                                  harris2graph
                               -------------------
-        begin                : 2015-03-02
-        git sha              : $Format:%H$
+        begin                : 2015-03-25
         copyright            : (C) 2015 by L - P: Heritage LLP
         copyright            : (C) 2015 by John Layt
         email                : john@layt.net
@@ -17,7 +14,7 @@
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
@@ -25,28 +22,18 @@
 
 import sys, os, argparse
 
+#Global data
 dataset = ''
 nodes = []
 edges = []
 nodeId = 0
 edgeId = 0
-boxWidth = 50
-boxHeight = 25
+nodeWidth = 50.0
+nodeHeight = 25.0
+graph = 'gml'
 
 def parseValueList(input, tag):
     return input[len(tag):].strip().replace(',', ' ').split()
-
-def setNode(node, unit):
-    global nodeId
-    if node:
-        nodes.append({'id': nodeId, 'label': node, 'unit': unit})
-        nodeId += 1
-
-def setEdges(sourceId, sourceLabel, targets):
-    global edgeId
-    for targetLabel in targets:
-        edges.append({'id': edgeId, 'source': sourceId, 'sourceLabel': sourceLabel, 'target': -1, 'targetLabel': targetLabel})
-        edgeId += 1
 
 def nodeIdForLabel(label):
     for node in nodes:
@@ -54,11 +41,49 @@ def nodeIdForLabel(label):
             return node['id']
     return -1
 
+def getNode(id):
+    for node in nodes:
+        if node['id'] == id:
+            return node
+
+def setNode(node, unit, indegree, outdegree):
+    global nodeId
+    if node:
+        nodes.append({'id': nodeId, 'label': node, 'unit': unit, 'indegree': indegree, 'outdegree': outdegree})
+        nodeId += 1
+
+def setEdges(sourceId, sourceLabel, targets):
+    global edgeId
+    for targetLabel in targets:
+        edges.append({'id': edgeId, 'source': sourceId, 'sourceLabel': sourceLabel, 'target': -1, 'targetLabel': targetLabel, 'weight': 1})
+        edgeId += 1
+
+def weightEdges():
+    for edge in edges:
+        if getNode(edge['source'])['outdegree'] == 1:
+            edge['weight'] = edge['weight'] + 2
+        if getNode(edge['target'])['indegree'] == 1:
+            edge['weight'] = edge['weight'] + 2
+
+def outEdges(id):
+    outedges = []
+    for edge in edges:
+        if edge['source'] == id:
+            outedges.append(edge)
+    return outedges
+
+def inEdges(id):
+    inedges = []
+    for edge in edges:
+        if edge['target'] == id:
+            inedges.append(edge)
+    return inedges
+
 def readLst(file):
     line = file.readline().strip()
     if (line is None or not line.startswith('Stratigraphic Dataset')):
-        print 'Invalid Header Line'
-        return
+        sys.stderr.write('Invalid Header Line\n')
+        return False
     dataset = line.strip()[len('Stratigraphic Dataset'):].strip()
     if args.name:
         dataset = args.name
@@ -66,12 +91,12 @@ def readLst(file):
         dataset = 'harris'
     line = file.readline().strip()
     if (line is None or line != ''):
-        print 'Missing Blank Line'
-        return
+        sys.stderr.write('Missing Blank Line')
+        return False
     line = file.readline().strip()
     if (line is None or line != 'Name'):
-        print 'Invalid Name Line'
-        return
+        sys.stderr.write('Invalid Name Line')
+        return False
     context = ''
     above = []
     contemporary = []
@@ -84,7 +109,7 @@ def readLst(file):
         elif (not line.startswith(' ')):
             if (context):
                 setEdges(nodeId, context, below)
-                setNode(context, unit)
+                setNode(context, unit, len(above), len(below))
             context = line.strip()
             above = []
             contemporary = []
@@ -106,24 +131,29 @@ def readLst(file):
 
     if context:
         setEdges(nodeId, context, below)
-        setNode(context, unit)
+        setNode(context, unit, len(above), len(below))
 
     for edge in edges:
         edge['target'] = nodeIdForLabel(edge['targetLabel'])
 
 
 def writeGv(simple):
-    print 'digraph' + dataset.replace(' ', '_') + '{'
+    print 'digraph ' + dataset.replace(' ', '_') + ' {'
     if not simple:
+        weightEdges()
         print '    splines=polyline' # Should be ortho but ports support not implemented
         print '    concentrate=true'
         print '    ranksep="1.0 equally"'
         print '    nodesep="2.0 equally"'
         print '    node [shape=box]'
-        print '    edge [arrowhead=none headport=n tailport=s width=' + str(boxWidth) + ' height=' + str(boxHeight) + ']'
+        print '    edge [arrowhead=none headport=n tailport=s width=' + str(nodeWidth) + ' height=' + str(nodeHeight) + ']'
 
     for edge in edges:
-        print '    ' + '"' + str(edge['sourceLabel']) + '"' + ' -> ' + '"' + str(edge['targetLabel']) + '";'
+        if simple:
+            print '    "' + str(edge['sourceLabel']) + '" -> "' + str(edge['targetLabel']) + '";'
+        else:
+            print '    "' + str(edge['sourceLabel']) + '" -> "' + str(edge['targetLabel']) + '" [weight=' + str(edge['weight']) + '];'
+
 
     print '}'
 
@@ -139,8 +169,8 @@ def writeGml(simple):
         if not simple:
             print '        graphics ['
             print '            type "rectangle"'
-            print '            w 50.0'
-            print '            h 25.0'
+            print '            w ' + str(nodeWidth)
+            print '            h ' + str(nodeHeight)
             print '        ]'
         print '    ]'
 
@@ -189,26 +219,33 @@ def writeGraphML(simple):
 parser = argparse.ArgumentParser(description='A tool to convert legacy .LST Harris Matrix files into modern graph formats.')
 parser.add_argument("-g", "--graph", help="Choose output graph format, optional, defaults to outfile suffix", choices=['gv', 'dot', 'gml', 'graphml'])
 parser.add_argument("-n", "--name", help="Name for graph")
+parser.add_argument("-wn", "--width", help="Width of node", type=float)
+parser.add_argument("-hn", "--height", help="Height of node", type=float)
 parser.add_argument("-s", "--simple", help="Only output basic node/edge data", action='store_true')
 parser.add_argument('infile', help="Source .lst file", nargs='?', type=argparse.FileType('r'), default=sys.stdin)
 parser.add_argument('outfile', help="Destination graph file", nargs='?', type=argparse.FileType('w'), default=sys.stdout)
 
 args = parser.parse_args()
 
-graph = 'gml'
 if args.graph:
     graph = args.graph
 elif args.outfile.name != '<stdout>':
     basename, graph = os.path.splitext(args.outfile.name)
     graph = graph.strip('.')
-
 graph = graph.lower()
+
+if (args.width is not None and args.width > 0):
+    nodeWidth = args.width
+if (args.height is not None and args.height > 0):
+    nodeHeight = args.height
+
 readLst(args.infile)
+
+if (args.name is not None and args.name):
+    dataset = args.name
 
 old_stdout = sys.stdout
 sys.stdout = args.outfile
-#if (args.output):
-#    sys.stdout = open(args.outfile, 'w')
 
 if (graph == 'gv' or graph == 'dot'):
     writeGv(args.simple)
@@ -217,5 +254,4 @@ elif (graph == 'graphml'):
 else:
     writeGml(args.simple)
 
-#if (args.output):
 sys.stdout = old_stdout
